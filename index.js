55e221219344c09548dc36d335bf24f881f92eff
Raman Karki <ramankarki40@gmail.com>
2022-09-04 20:59:42 +0545

    update

--- index.js
+++ index.js
@@ -6,16 +6,31 @@ const { exec, fork } = require('child_process')
 let FgYellow = '\x1b[33m',
   Reset = '\x1b[0m',
   FgGreen = '\x1b[32m',
-  FgRed = '\x1b[31m',
   package = '[relod]',
-  file = process.argv[2],
+  command = process.argv[2],
   setTimeoutId,
   childProcess
 
-if (!file)
-  return console.error(FgRed + package, 'Enter: relod filename.extension')
+if (
+  !command ||
+  command === '-v' ||
+  command === '--version' ||
+  command === '--help'
+) {
+  console.log('\nrelod <command>')
+  console.log('relod @1.0.0\n')
+  console.log('Usage:\n')
+  console.log(
+    'relod filename \t Entry file name of your node server. Same as you use in nodemon.'
+  )
+  console.log('relod --help \t Get help.')
+  console.log('relod -v \t Get version.')
+  console.log('relod --version  Get version.')
 
-console.log(FgYellow + package, 'Watching file recursively:', file)
+  return
+}
+
+console.log(FgYellow + package, 'Watching file recursively:', command)
 
 // search for all the files and start watching
 chokidar.watch('.').on('all', (event, path) => {
@@ -30,12 +45,12 @@ chokidar.watch('.').on('all', (event, path) => {
   setTimeoutId = setTimeout(() => {
     if (!childProcess) {
       console.log(FgGreen + package + ' Files loaded.')
-      console.log(package, 'Starting `node', file + '`')
+      console.log(package, 'Starting `node', command + '`')
     }
 
     childProcess?.kill()
     console.log(Reset)
-    childProcess = fork(file)
+    childProcess = fork(command)
   }, 500)
 })
 //
